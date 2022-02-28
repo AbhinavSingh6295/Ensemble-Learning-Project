f41efe42d46ade98c5951a37db7f8972474796f5
dfroca <df.roca956@gmail.com>
2022-02-28 15:08:09 +0100

    Update the structure and create the main file structure

---
+++ DecisionTree/DecisionTreeImplementation.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 14:21:31 2022
+
+@author: Abhinav Singh
+"""
+
+# Imports
+import numpy as np
+# from sklearn.datasets import make_moons
+
+# This is the main function of the decision tree algorithm
+def decision_tree(data, count=0, min_samples=2, max_depth=5):
+    print("To implement")
+    return 0
+
+# -------------------------------------
+# Classification Test Data
+# -------------------------------------
+# N = 1000
+# X,y = make_moons(N, noise = 0.2)
+
+# -------------------------------------
+# Regression Test Data
+# -------------------------------------
+# N = 200
+# X = np.linspace(-1,1,N)
+# Y = X**2 + np.random.normal(0,0.07,N)
+# X = X.reshape(-1, 1)
+
+# Calls to the main function
+data = 1
+decision_tree(data)
--- DecisionTreeImplementation.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 28 14:21:31 2022
-
-@author: Abhinav Singh
-"""
-
